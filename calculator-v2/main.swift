1ad2a300a86dc5ea15d5afd2f6c6e1e2e055b25f
Nick-Nordale <nicknordale@gmail.com>
2016-04-05 21:23:52 -0700

    Initial Commit

---
+++ calculator-v2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  calculator-v2
+//
+//  Created by Nick Nordale on 4/5/16.
+//  Copyright © 2016 Nick Nordale. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
