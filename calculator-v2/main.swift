185cef8a3be4c437cf54dc9be09f54fe0651dcab
Nick-Nordale <nicknordale@gmail.com>
2016-04-05 22:00:31 -0700

    done

--- calculator-v2/main.swift
+++ calculator-v2/main.swift
@@ -8,5 +8,58 @@
 
 import Foundation
 
-print("Hello, World!")
+func add(first : Int, second : Int) -> Int {
+    return first + second
+}
 
+func subtract(first : Int, second : Int) -> Int {
+    return first - second
+}
+
+func multiply(first : Int, second : Int) -> Int {
+    return first * second
+}
+
+func divide(first : Int, second : Int) -> Int {
+    return first / second
+}
+
+func math(first : Int, second : Int, op : (Int, Int) -> Int) -> Int {
+    return op(first, second)
+}
+
+func addArr(arrIn : [Int]) -> Int {
+    var out = 0
+    for index in 0...(arrIn.count - 1) {
+        out += arrIn[index]
+    }
+    return out
+}
+
+func multiplyArr(arrIn : [Int]) -> Int {
+    var out = 1
+    for index in 0...(arrIn.count - 1) {
+        out = out * arrIn[index]
+    }
+    return out
+}
+
+func count(arrIn : [Int]) -> Int {
+    var out = 0
+    for _ in 0...(arrIn.count - 1) {
+        out += 1
+    }
+    return out
+}
+
+func avg(arrIn : [Int]) -> Int {
+    var tot = 0
+    for index in 0...(arrIn.count - 1) {
+        tot += arrIn[index]
+    }
+    return tot/arrIn.count
+}
+
+func math2(arrIn : [Int], op : ([Int]) -> Int) -> Int {
+    return op(arrIn)
+}
